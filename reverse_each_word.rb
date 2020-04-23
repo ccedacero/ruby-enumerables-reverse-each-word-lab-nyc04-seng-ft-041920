64bb63ccfe572380a4afd47a8cac49f4e81dd4d5
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-23 15:14:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,11 @@
 def reverse_each_word(str)
   str_Arr = str.split(' ')
-  new_str = ""
+  arr = []
   str_Arr.each do  |word|
-   new_str += word.reverse() + ' '
+  arr << word.reverse()
   end
-  new_str
+    arr.join(' ')
 end
-
 #   def reverse_each_word(str)
 #   str_Arr = str.split(' ')
 #   new_str = str_Arr.collect { |word| word.reverse() } 
